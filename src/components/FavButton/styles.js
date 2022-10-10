8901cd41a8679602fff14708f65d5b669e96e808
Camilo Puentes <jcpsandoval94@gmail.com>
2022-10-10 18:51:08 -0500

    Se realiza aplicacion de petgram con vercel

---
+++ src/components/FavButton/styles.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components'
+
+export const Button = styled.button`
+  display: flex;
+  align-items: center;
+  padding-top: 5px;
+  padding-bottom: 7px;
+  & svg {
+    margin-right: 4px;
+  }
+`
